11106b6d042e86625ba97df4b781d78ce8072a54
darshanpanse <darsh.panse@gmail.com>
2018-05-14 11:00:32 +0530

    added all paths handler to server

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'soffra/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 console.log(`Soffra server listening on ${port}`);
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'soffra/build')));
 
 // require('./climbDataDBConn')(app);
 require('./DBConnection')();

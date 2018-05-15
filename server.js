4992bb024ee2899b8222be9c2c670575542ce61e
darshanpanse <panse.d@husky.neu.edu>
2018-05-15 05:21:41 -0400

    moved catchall handler to the bottom

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'soffra/build')));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname+'/soffra/build/index.html'));
-//   });
-
 const port = process.env.PORT || 5000;
 app.listen(port);
 console.log(`Soffra server listening on ${port}`);
@@ -22,3 +16,9 @@ console.log(`Soffra server listening on ${port}`);
 // require('./climbDataDBConn')(app);
 require('./DBConnection')();
 require('./app')(app);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/soffra/build/index.html'));
+  });
